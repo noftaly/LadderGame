50d5b007528e347badf7a7e14b539d010753b04f
Elliot Maisl <elliot.maisl@gmail.com>
2020-12-07 16:35:17 +0100

    🔥 Remove unused old code

--- Sources/main.py
+++ Sources/main.py
@@ -5,18 +5,9 @@ window = tkinter.Tk()
 
 from Sources.game import Game
 
-level = 1
-finished = False
-cells = []
-chests = []
-
 window.title('LadderGame')
 
 canvas = tkinter.Canvas(window, width=21*32, height=21*32)
-
-knight = None
-skeleton = None
-
 canvas.focus_set()
 
 game = Game(window, canvas)
